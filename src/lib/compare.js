a02ea4fecb11df492bed25da1a64bc89af9354e1
Valentin Evteev <stratumforce@protonmail.com>
2017-05-26 14:38:10 +0300

    add abstraction layer

--- src/lib/compare.js
+++ src/lib/compare.js
@@ -6,7 +6,7 @@ import parseContent from './contentParser';
 const getFileContent = (config) => {
   const fileContent = fs.readFileSync(config, 'utf-8');
   const fileExt = path.extname(config).slice(1);
-  return parseContent[fileExt](fileContent);
+  return parseContent(fileExt, fileContent);
 };
 
 export default(firstConfig, secondConfig) => {
--- src/lib/contentParser.js
+++ src/lib/contentParser.js
@@ -1,10 +1,12 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parseContent = {
+const argTypes = {
   json: arg => JSON.parse(arg),
   yml: arg => yaml.safeLoad(arg),
   ini: arg => ini.parse(arg),
 };
+
+const parseContent = (type, content) => argTypes[type](content);
 
 export default parseContent;
